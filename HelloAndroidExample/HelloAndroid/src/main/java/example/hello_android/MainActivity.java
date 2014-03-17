9a270a960e0da00d9422f427b16f5ed6d4a24ff8
Joe Rowley Laptop <joe.rowley@bodybuilding.com>
2014-03-16 19:31:36 -0600

    Setting up a more complex layout and consuming elements in the activity

--- HelloAndroidExample/HelloAndroid/src/main/java/example/hello_android/MainActivity.java
+++ HelloAndroidExample/HelloAndroid/src/main/java/example/hello_android/MainActivity.java
@@ -1,17 +1,46 @@
 package example.hello_android;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+    TextView label;
+    EditText input;
+    Button inputAction, explicitIntent, implicitIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        label = (TextView)findViewById(R.id.hello_world_label);
+        input = (EditText)findViewById(R.id.input_field);
+
+        inputAction = (Button)findViewById(R.id.what_happens_button);
+        explicitIntent = (Button)findViewById(R.id.explicit_intent);
+        implicitIntent = (Button)findViewById(R.id.implicit_intent);
+
+        inputAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(input.getText().length() > 0) {
+                    label.setText(input.getText().toString());
+                }
+                input.setText(null);
+            }
+        });
+
+        explicitIntent.setOnClickListener(handleExplicitIntent);
+        implicitIntent.setOnClickListener(this);
     }
 
     @Override
@@ -27,5 +56,29 @@ public class MainActivity extends Activity {
     @Override
     public void onDestroy() {
         super.onDestroy();
+    }
+
+    public void enableIntents(View view) {
+        explicitIntent.setEnabled(true);
+        implicitIntent.setEnabled(true);
+        view.setEnabled(false);
+    }
+
+    Button.OnClickListener handleExplicitIntent = new Button.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //todo
+            Log.d("TEST", "handleExplicitIntent clicked");
+        }
+    };
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.implicit_intent:
+                //todo
+                Log.d("TEST", "implicitIntent clicked");
+                break;
+        }
     }
 }
